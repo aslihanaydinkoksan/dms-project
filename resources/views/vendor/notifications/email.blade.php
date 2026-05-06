7eda666255b105f12f900886bc6a81d953402690
Aslihan Aydin <aslihan.aydin@koksan.com>
2026-05-06 16:21:37 +0300

    fix

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,53 +1,55 @@
 <x-mail::message>
-    {{-- Greeting --}}
-    @if (!empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # Eyvah Bir Hata Oluştu!
-        @else
-            # Merhaba!
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# Eyvah Bir Hata Oluştu!
+@else
+# Merhaba!
+@endif
+@endif
 
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        $color = match ($level) {
-            'success', 'error' => $level,
-            default => 'primary',
-        };
-        ?>
-        <x-mail::button :url="$actionUrl" :color="$color">
-            {{ $actionText }}
-        </x-mail::button>
-    @endisset
+@endforeach
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    if ($level === 'success' || $level === 'error') {
+        $color = $level;
+    } else {
+        $color = 'primary';
+    }
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
 
-    {{-- Salutation --}}
-    @if (!empty($salutation))
-        {{ $salutation }}
-    @else
-        İyi çalışmalar dileriz,<br>
-        {{ config('app.name') }}
-    @endif
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        <x-slot:subcopy>
-            "{{ $actionText }}" butonuna tıklamakta sorun yaşıyorsanız, aşağıdaki bağlantıyı kopyalayıp web tarayıcınıza
-            yapıştırın:
+@endforeach
 
-            <span class="break-all">[{{ $actionUrl }}]({{ $actionUrl }})</span>
-        </x-slot:subcopy>
-    @endisset
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+İyi çalışmalar dileriz,<br>
+{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+<x-slot:subcopy>
+"{{ $actionText }}" butonuna tıklamakta sorun yaşıyorsanız, aşağıdaki bağlantıyı kopyalayıp web tarayıcınıza yapıştırın:
+
+<span class="break-all">[{{ $actionUrl }}]({{ $actionUrl }})</span>
+</x-slot:subcopy>
+@endisset
 </x-mail::message>
